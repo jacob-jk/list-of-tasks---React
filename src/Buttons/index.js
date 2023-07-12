e7d6cdad253116a1fffedad3e1813d2e306f8c13
Kuba Karnath <jacob_jk@wp.pl>
2023-07-12 20:58:57 +0200

    Buttons spelling correction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,20 +1,18 @@
 import "./style.css";
 
-const Buttons = (props) => {
-  if (props.tasks.lenght !== 0)
-    return (
-      <div className="buttons">
-        <button className="buttons">
-          {props.hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </button>
-        <button
-          className="buttons"
-          disabled={props.tasks.every(({ done }) => done)}
-        >
-          Ukończ wszystkie
-        </button>
-      </div>
-    );
-};
+const Buttons = (props) =>
+  props.tasks.length > 0 && (
+    <div className="buttons">
+      <button className="buttons">
+        {props.hideDone ? "Pokaż" : "Ukryj"} ukończone
+      </button>
+      <button
+        className="buttons"
+        disabled={props.tasks.every(({ done }) => done)}
+      >
+        Ukończ wszystkie
+      </button>
+    </div>
+  );
 
 export default Buttons;
