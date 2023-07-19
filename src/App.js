c44deb1584231f60df80f7c33d11204dd8ff3310
Kuba Karnath <jacob_jk@wp.pl>
2023-07-19 11:55:30 +0200

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import hello, { surname, age } from "./utils/hello";
+// można też zamiast wszystkich nazw obiektu dać: * as hello
+
+hello.hello();
+console.log(hello.name);
+console.log(surname, age);
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
   { id: 2, content: "zjeść kolację", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
   return (
     <Container>
       <Header tittle="Lista zadań" />
@@ -22,7 +33,13 @@ function App() {
       <Section
         tittle="Lista zadań"
         listContent={<List tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
+        }
       />
     </Container>
   );
