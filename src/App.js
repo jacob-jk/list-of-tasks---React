25181977803a3dd1b57f4b9ecd93952b8928ef6f
Kuba Karnath <jacob_jk@wp.pl>
2024-04-15 14:20:20 +0200

    useTasks hook created

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Container from "./Container";
+import { useTasks } from "./useTasks";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(
-    localStorage.getItem("tasks")
-      ? JSON.parse(localStorage.getItem("tasks"))
-      : [
-          { id: 1, content: "przejść na Reacta", done: false },
-          { id: 2, content: "zjeść kolację", done: true },
-        ]
-  );
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
   };
 
-  const removeTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleTask = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      )
-    );
-  };
-
-  const setAllDone = () => {
-    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (newTaskContent) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content: newTaskContent,
-        done: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ]);
-  };
+  const { tasks, removeTask, toggleTask, setAllDone, addNewTask } = useTasks();
 
   return (
     <Container tittle="Lista zadań">
