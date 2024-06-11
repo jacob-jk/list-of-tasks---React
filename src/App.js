8fd72b45e2b5190c481253f08ff16135c7ab990d
Kuba Karnath <jacob_jk@wp.pl>
2024-06-11 12:10:24 +0200

    application split into features

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
-import { useState } from "react";
-import Form from "./Form";
-import List from "./List";
-import Buttons from "./Buttons";
-import Section from "./Section";
-import Container from "./Container";
-import { useTasks } from "./useTasks";
+import Tasks from "./features/tasks/Tasks";
 
 function App() {
-  const [hideDone, setHideDone] = useState(false);
-
-  const toggleHideDone = () => {
-    setHideDone((hideDone) => !hideDone);
-  };
-
-  const { tasks, removeTask, toggleTask, setAllDone, addNewTask } = useTasks();
-
-  return (
-    <Container tittle="Lista zadań">
-      <Section
-        tittle="Dodaj nowe zadanie"
-        listContent={<Form addNewTask={addNewTask} />}
-      />
-
-      <Section
-        tittle="Lista zadań"
-        listContent={
-          <List
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTask={toggleTask}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
-      />
-    </Container>
-  );
+  return <Tasks />;
 }
 
 export default App;
---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Form from "./Form";
+import List from "./List";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Container from "../../common/Container";
+import { useTasks } from "./useTasks";
+
+function Tasks() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
+  const { tasks, removeTask, toggleTask, setAllDone, addNewTask } = useTasks();
+
+  return (
+    <Container tittle="Lista zadań">
+      <Section
+        tittle="Dodaj nowe zadanie"
+        listContent={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        tittle="Lista zadań"
+        listContent={
+          <List
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTask={toggleTask}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
