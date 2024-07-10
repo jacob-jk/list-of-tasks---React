8b7204f5e916eead55274d3fcecff2944b20e16d
Kuba Karnath <jacob_jk@wp.pl>
2024-07-10 12:18:30 +0200

    local storage applied

---
+++ src/features/tasks/tasksLocalStorage.js
@@ -0,0 +1,6 @@
+export const saveTasksInLocalStorage = (tasks) => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+export const getTasksFromLocalStorage = () =>
+  JSON.parse(localStorage.getItem("tasks")) || [];
---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeEvery, call, put, takeLatest, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setExampleTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+  try {
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setExampleTasks(exampleTasks));
+  } catch (error) {
+    yield call(alert, "coś poszło nie tak!");
+  }
+}
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
